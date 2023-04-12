a38f40bf381e0fa184bc756de9b1f122978af5b1
Wellinton Alexandre <wellinton1067@gmail.com>
2023-04-12 19:12:41 -0300

    typescript exercise

---
+++ src/index.js
@@ -0,0 +1,2 @@
+var multiply = function (n, n2) { return n * n2; };
+console.log(multiply(2, 2));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const multiply = (n: number, n2: number): number => n * n2;
+const greeting = (name: string): string =>  `Ola ${name}`;
+
+
+console.log(multiply(2, 2));
+console.log(greeting('Wellinton'));
